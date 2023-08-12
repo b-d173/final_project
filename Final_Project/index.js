a3617c9e080a1de0eef185313057ec576abc2bbf
b-d173 <138835231+b-d173@users.noreply.github.com>
2023-08-12 12:59:44 +1000

    Add files via upload

---
+++ Final_Project/index.js
@@ -0,0 +1,10 @@
+
+
+// html, event listener, function
+
+var hamburgerIcon = document.querySelector('#hamburgericon')
+hamburgerIcon.addEventListener('click', showHamburgerMenu) 
+
+  function showHamburgerMenu() {
+    console.log('hello')
+  }
